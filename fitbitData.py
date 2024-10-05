d7fd94251c287d8e918f7588328b672b919d23e6
K-0123 <daebakarmyforever@gmail.com>
2024-10-05 12:40:13 -0700

    first commit

---
+++ fitbitData.py
@@ -0,0 +1,4 @@
+import fitbit
+
+authd_client = fitbit.Fitbit('23PTT3', 'df91fff89d626d585fccc8c0e03a828e',
+                             access_token='<access_token>', refresh_token='<refresh_token>')
