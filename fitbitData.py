115225bff96ee535dceb7ed986a7813e7d24f47c
K-0123 <daebakarmyforever@gmail.com>
2024-10-05 16:51:40 -0700

    code to get fitbit tokens to acces api

--- fitbitData.py
+++
@@ -1,4 +0,0 @@
-import fitbit
-
-authd_client = fitbit.Fitbit('23PTT3', 'df91fff89d626d585fccc8c0e03a828e',
-                             access_token='<access_token>', refresh_token='<refresh_token>')
